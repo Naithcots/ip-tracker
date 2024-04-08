5742e6f32a04a381bb1fbf806f193e0662b8a7f4
Naithcots <sebastian.stochmial2000@gmail.com>
2024-04-08 17:12:09 +0200

    Add desktop styles, update grid and font sizes

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ const App = () => {
 
   useEffect(() => {
     if (mapRef?.current && !map) {
-      var _map = leaflet.map(mapRef.current, {zoomControl: false}).setView([51.505, -0.09], 13);
+      var _map = leaflet
+        .map(mapRef.current, { zoomControl: false })
+        .setView([51.505, -0.09], 13);
       leaflet
         .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
           maxZoom: 19,
@@ -20,69 +22,70 @@ const App = () => {
     }
 
     return () => {
-      _map.off();
-      _map.remove();
+      if (_map) {
+        _map.off();
+        _map.remove();
+      }
     };
   }, [mapRef]);
 
   return (
-    <div className="grid max-h-dvh grid-cols-1 grid-rows-3">
-      <div className="col-[1/1] row-[1/2]">
-        <img
-          src="/images/pattern-bg-mobile.png"
-          alt="background"
-          className="w-full object-cover"
-        />
-      </div>
-      <section className="col-[1/1] row-[1/3]">
-        <div className="px-6">
-          <header className="my-6 text-center">
-            <h1 className="text-3xl font-medium text-white">
-              IP Address Tracker
-            </h1>
-          </header>
-          <section>
-            <div className="flex h-12">
-              <input
-                type="text"
-                className="w-full rounded-l-xl px-5 py-3 outline-none"
-              />
-              <button className="bg-veryDarkGray hover:bg-veryDarkGray/70 grid aspect-square h-full place-items-center rounded-r-xl transition-colors">
-                <img src="/images/icon-arrow.svg" />
-              </button>
+    <div className="grid h-dvh grid-rows-[auto_auto_auto_1fr]">
+      <div className="col-[1/1] row-[1/3] bg-[url('/images/pattern-bg-mobile.png')] bg-cover bg-no-repeat md:bg-[url('/images/pattern-bg-desktop.png')]" />
+      <section className="col-[1/1] row-[1/1] mx-auto mb-8 w-full max-w-7xl px-6 md:mb-10">
+        <header className="py-6 text-center">
+          <h1 className="text-3xl font-medium text-white">
+            IP Address Tracker
+          </h1>
+        </header>
+        <section className="flex justify-center">
+          <div className="flex h-12 max-w-2xl grow">
+            <input
+              type="text"
+              className="w-full rounded-l-xl px-5 py-3 outline-none"
+            />
+            <button className="bg-veryDarkGray hover:bg-veryDarkGray/70 grid aspect-square h-full place-items-center rounded-r-xl transition-colors">
+              <img src="/images/icon-arrow.svg" />
+            </button>
+          </div>
+        </section>
+      </section>
+      <section className="col-[1/1] row-[2/4]">
+        <div className="mx-auto max-w-7xl px-6">
+          <div className="relative z-10 flex flex-col gap-4 rounded-xl bg-white p-7 shadow-sm md:flex-row md:justify-around md:py-9">
+            <div className="space-y-1 text-center md:text-left">
+              <p className="text-darkGray text-sm font-bold tracking-widest">
+                IP ADDRESS
+              </p>
+              <p className="text-xl font-medium sm:text-2xl">192.212.174.101</p>
             </div>
-          </section>
-          <section className="relative z-10">
-            <div className="mt-5 flex flex-col gap-4 rounded-xl bg-white p-5">
-              <div className="space-y-1 text-center">
-                <p className="text-darkGray text-sm font-bold tracking-widest">
-                  IP ADDRESS
-                </p>
-                <p className="text-xl font-medium">192.212.174.101</p>
-              </div>
-              <div className="space-y-1 text-center">
-                <p className="text-darkGray text-sm font-bold tracking-widest">
-                  LOCATION
-                </p>
-                <p className="text-xl font-medium">Brooklyn, NY 10001</p>
-              </div>
-              <div className="space-y-1 text-center">
-                <p className="text-darkGray text-sm font-bold tracking-widest">
-                  TIMEZONE
-                </p>
-                <p className="text-xl font-medium">UTC -05:00</p>
-              </div>
-              <div className="space-y-1 text-center">
-                <p className="text-darkGray text-sm font-bold tracking-widest">
-                  ISP
-                </p>
-                <p className="text-xl font-medium">SpaceX Starlink</p>
-              </div>
+            <div className="bg-darkGray/40 w-[1px]" />
+            <div className="space-y-1 text-center md:text-left">
+              <p className="text-darkGray text-sm font-bold tracking-widest">
+                LOCATION
+              </p>
+              <p className="text-xl font-medium sm:text-2xl">
+                Brooklyn, NY 10001
+              </p>
             </div>
-          </section>
+            <div className="bg-darkGray/40 w-[1px]" />
+            <div className="space-y-1 text-center md:text-left">
+              <p className="text-darkGray text-sm font-bold tracking-widest">
+                TIMEZONE
+              </p>
+              <p className="text-xl font-medium sm:text-2xl">UTC -05:00</p>
+            </div>
+            <div className="bg-darkGray/40 w-[1px]" />
+            <div className="space-y-1 text-center md:text-left">
+              <p className="text-darkGray text-sm font-bold tracking-widest">
+                ISP
+              </p>
+              <p className="text-xl font-medium sm:text-2xl">SpaceX Starlink</p>
+            </div>
+          </div>
         </div>
       </section>
-      <section className="z-0 col-[1/1] row-[2/4]">
+      <section className="z-0 col-[1/1] row-[3/-1]">
         <div id="map" className="h-full" ref={mapRef}></div>
       </section>
     </div>
